c5e52ac7f14978b287319c38f21e0e0683db6c4c
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-16 00:24:47 +0530

    a

--- server.js
+++ server.js
@@ -6,9 +6,23 @@ const sendGrid = require('@sendgrid/mail');
 
 
 const app = express();
-
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
 
 app.use(cors());
 
@@ -53,7 +67,3 @@ app.post('/api/email', (req, res, next) => {
 
         });
 });
-
-
-const port = process.env.PORT || 5000;
-app.listen(port);
