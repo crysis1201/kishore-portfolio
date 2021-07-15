f105f766d8b0ebf25178d9f74c6785cb535d4214
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-15 22:52:57 +0530

    a

---
+++ src/redux/portfolio/portfolio.reducer.js
@@ -0,0 +1,20 @@
+
+
+const INITIAL_STATE = {
+    porttfolio: null
+}
+
+const portfolioReducer = ({ state = INITIAL_STATE, action }) =>{
+    switch (action.types) {
+        case 'SET_PORTFOLIO' :
+            return {
+                ...state,
+                porttfolio: action.payload
+            }
+
+        default:
+            return state
+    };
+};
+
+export default portfolioReducer
