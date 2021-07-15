ec50a309b3ad2b6c3bbac5aeb026eaf76eeefd7f
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-15 23:00:37 +0530

    a

---
+++ src/redux/portfolio/portfolio_data.js
@@ -0,0 +1,74 @@
+const PORTFOLIO_DATA = {
+    flutter: {
+      id: 1,
+      title: 'Flutter Apps',
+      routeName: 'flutter',
+      items: [
+        {
+          id: 1,
+          name: 'Brown Brim',
+          imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'Blue Beanie',
+          imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+          price: 18
+        },
+        {
+          id: 3,
+          name: 'Brown Cowboy',
+          imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+          price: 35
+        },
+      ]
+    },
+    motionDesign: {
+      id: 2,
+      title: 'Motion Graphics',
+      routeName: 'motiongraphics',
+      items: [
+        {
+          id: 10,
+          name: 'Adidas NMD',
+          imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+          price: 220
+        },
+        {
+          id: 11,
+          name: 'Adidas Yeezy',
+          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+          price: 280
+        },
+      ]
+    },
+    video :{
+      id: 5,
+      title: 'Video Works',
+      routeName: 'videoworks',
+      items: [
+        {
+          id: 30,
+          name: 'Camo Down Vest',
+          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+          price: 325
+        },
+        {
+          id: 31,
+          name: 'Floral T-shirt',
+          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+          price: 20
+        },
+        {
+          id: 32,
+          name: 'Black & White Longsleeve',
+          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+          price: 25
+        },
+      ]
+    }
+  };
+  
+  export default PORTFOLIO_DATA;
+  
