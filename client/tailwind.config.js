a85f3879b44c97f10c3f933365faa385e90ffe4b
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-23 23:46:15 +0530

    added purge css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      './src/components/header/introCard.jsx',
+      './src/components/header/header.jsx',
+      './src/components/portfolio-content/content.jsx',
+      './src/components/portfolio-content/portfolioContent.jsx',
+      './src/components/portfolio-overview/portfolioOverview.jsx',
+      './src/components/portfolio-overview/portfolioPreview.jsx',
+      './src/components/progressDiv/progressDiv.jsx',
+      './src/components/menu.jsx',
+      './src/pages/ContactPage.jsx',
+      './src/pages/homepage.jsx',
+      './src/pages/portfolio.jsx',
+      './src/pages/portfolioContentPage.jsx',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
