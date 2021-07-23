a794bf0f4512715503158f4898765ddbec733614
Eazhilamuthan <eazhilamuthan12@gmail.com>
2021-07-24 00:05:40 +0530

    turned purge css off need to work on that

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,5 @@
 module.exports = {
-  purge: {
-    content: [
-      './src/components/header/introCard.jsx',
-      './src/components/header/header.jsx',
-      './src/components/portfolio-content/content.jsx',
-      './src/components/portfolio-content/portfolioContent.jsx',
-      './src/components/portfolio-overview/portfolioOverview.jsx',
-      './src/components/portfolio-overview/portfolioPreview.jsx',
-      './src/components/progressDiv/progressDiv.jsx',
-      './src/components/menu.jsx',
-      './src/pages/ContactPage.jsx',
-      './src/pages/homepage.jsx',
-      './src/pages/portfolio.jsx',
-      './src/pages/portfolioContentPage.jsx',
-    ]
-  },
+  purge: [false],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
